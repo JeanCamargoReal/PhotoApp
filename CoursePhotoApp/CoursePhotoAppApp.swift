f8d4252429e1fb0797b788f351aa666fd1f11583
Jean Camargo <jeancamargoreal@icloud.com>
2023-12-02 18:32:10 -0300

    Initial Commit

---
+++ CoursePhotoApp/CoursePhotoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoursePhotoAppApp.swift
+//  CoursePhotoApp
+//
+//  Created by Jean Camargo on 02/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CoursePhotoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
