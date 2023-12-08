d9589de31bf8cbbb3b15beafba13766f80389dec
Jean Camargo <jeancamargoreal@icloud.com>
2023-12-08 15:55:29 -0300

    Desenvolvendo o card

--- CoursePhotoApp/ContentView.swift
+++ CoursePhotoApp/ContentView.swift
@@ -9,16 +9,93 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            VStack {
+                NavigationBarView()
+                VStack(alignment: .leading) {
+                    HStack {
+                        Image(.woman2)
+                            .resizable()
+                            .clipShape(RoundedRectangle(cornerRadius: 4))
+                            .frame(width: 36, height: 36)
+                        VStack(alignment: .leading) {
+                            Text("mille_f")
+                                .font(.footnote)
+                                .fontWeight(.bold)
+                            HStack {
+                                Text("London, England")
+                                    .font(.footnote)
+                                    .foregroundStyle(.secondary)
+                                Spacer()
+                                Text("2 minutes ago")
+                                    .font(.footnote)
+                                    .foregroundStyle(.secondary)
+                            }
+                        }
+                    }
+                    Image(.flower)
+                        .resizable()
+                        .frame(height: 300)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                        .shadow(color: .gray, radius: 10, x: 2, y: 5)
+                    
+                    HStack(spacing: 28) {
+                        Button(action: {}) {
+                            HStack {
+                                Image(systemName: "heart")
+                                    .font(.headline.weight(.semibold))
+                                Text("22")
+                                    .font(.caption)
+                            }.foregroundStyle(.black)
+                        }
+                        Button(action: {}) {
+                            HStack {
+                                Image(systemName: "bubble.right")
+                                    .font(.headline.weight(.semibold))
+                                Text("4")
+                                    .font(.caption)
+                            }.foregroundStyle(.black)
+                        }
+                    }.padding(.top)
+                }
+                .padding(.leading)
+                .padding(.trailing)
+                
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct NavigationBarView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Button(action: {}) {
+                    Image(systemName: "camera")
+                        .resizable()
+                        .frame(width: 25, height: 20)
+                }.foregroundStyle(.black)
+                Spacer()
+                Text("PhotoApp")
+                    .font(Font.custom("Billabong", size: 28))
+                Spacer()
+                Button(action: {}) {
+                    Image(uiImage: .woman2)
+                        .resizable()
+                        .renderingMode(.original)
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 36, height: 36)
+                        .clipShape(.circle)
+                        .shadow(color: .black.opacity(0.1), radius: 1, x: 0, y: 1)
+                        .shadow(color: .black.opacity(0.2), radius: 10, x: 0, y: 10)
+                }
+            }
+            .padding()
+        }
+    }
 }
